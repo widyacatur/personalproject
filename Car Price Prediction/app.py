b2c8f61b1008697bf33c18c54a152dc020524439
widyacatur <widyaramli@gmail.com>
2025-08-24 14:35:45 +0700

    edit

--- Car Price Prediction/app.py
+++ Car Price Prediction/app.py
@@ -17,8 +17,8 @@ def get_brand_name(car_name):
 cars_data['name'] = cars_data['name'].apply(get_brand_name)
 
 name = st.selectbox('Select Car Brand', cars_data['name'].unique())
-year = st.slider('Select Year', int(cars_data['year'].min()), int(cars_data['year'].max()))
-km_driven = st.slider('Select Kms Driven', int(cars_data['km_driven'].min()), int(cars_data['km_driven'].max()))
+year = st.slider('Select Year', 1994,2024)
+km_driven = st.slider('Select Kms Driven', 11,200000)
 fuel = st.selectbox('Select Fuel Type', cars_data['fuel'].unique())
 seller_type = st.selectbox('Select Seller Type', cars_data['seller_type'].unique())
 transmission = st.selectbox('Select Transmission Type', cars_data['transmission'].unique())
@@ -32,4 +32,21 @@ if st.button('Predict Price'):
     input_data_model = pd.DataFrame(
     [[name,year,km_driven,fuel,seller_type,transmission,owner,mileage,engine,max_power,seats]],
     columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner', 'mileage', 'engine', 'max_power', 'seats'])
-    st.write(input_data_model)
+
+    input_data_model['owner'].replace(['First Owner', 'Second Owner', 'Third Owner',
+       'Fourth & Above Owner', 'Test Drive Car'],
+                           [1,2,3,4,5], inplace=True)
+    input_data_model['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'],[1,2,3,4], inplace=True)
+    input_data_model['seller_type'].replace(['Individual', 'Dealer', 'Trustmark Dealer'],[1,2,3], inplace=True)
+    input_data_model['transmission'].replace(['Manual', 'Automatic'],[1,2], inplace=True)
+    input_data_model['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
+       'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
+       'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
+       'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
+       'Ambassador', 'Ashok', 'Isuzu', 'Opel'],
+                          [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+                          ,inplace=True)
+
+    car_price = model.predict(input_data_model)
+
+    st.markdown('Car Price is going to be '+ str(car_price[0]))
